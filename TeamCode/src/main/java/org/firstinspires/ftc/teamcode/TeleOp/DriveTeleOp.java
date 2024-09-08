fb48f6d1fc280b1be1c7effe966726cc879b72f8
jleung26 <jleung26@punahou.edu>
2024-09-07 18:11:00 -1000

    adding opmodes and instructions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/DriveTeleOp.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Claw;
+import org.firstinspires.ftc.teamcode.Subsystems.HorizontalSlides;
+import org.firstinspires.ftc.teamcode.Subsystems.Intake;
+import org.firstinspires.ftc.teamcode.Subsystems.Mecanum;
+import org.firstinspires.ftc.teamcode.Subsystems.OTOSManager;
+import org.firstinspires.ftc.teamcode.Subsystems.ScoringCombined;
+import org.firstinspires.ftc.teamcode.Subsystems.VerticalSlides;
+
+@TeleOp(name="Mecanum Drive with OTOS", group="Active TeleOps")
+public class DriveTeleOp extends OpMode {
+    // creating subsystems
+    private Mecanum mecanum = new Mecanum();
+    private OTOSManager otosManager = new OTOSManager();
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    @Override
+    public void init() {
+        mecanum.initialize(this);
+        otosManager.initialize(this, true);
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+
+    /*
+     * allows driver to indicate that the IMU should not be reset
+     * used when starting TeleOp after auto or if program crashes in the middle of match
+     * relevant because of field-centric controls
+     */
+    @Override
+    public void init_loop() {
+    }
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    @Override
+    public void start() {
+        telemetry.clear();
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    @Override
+    public void loop() {
+        mecanum.operateFieldCentric(); // toggle Y button for slow mode
+        otosManager.operate();
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+        mecanum.shutdown();
+    }
+}
+
