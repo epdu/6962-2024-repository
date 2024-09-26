a0a24fe910b8ff2eabcffc23855991f30c17df0e
nwilliams25 <nwilliams25@punahou.edu>
2024-09-26 13:56:44 -1000

    setup initial trajectories for auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TestAuto.java
@@ -21,8 +21,27 @@ import org.firstinspires.ftc.teamcode.Roadrunner.MecanumDrive;
 @Config
 @Autonomous(name = "Test Auto", group = "Test Autonomous")
 public class TestAuto extends LinearOpMode{
+
+    double startX = 0;
+    double startY = 0;
+    double startHeading = Math.toRadians(0);
+    double scorePreloadX = 0;
+    double scorePreloadY = 0;
+    double pickupX = 0;
+    double pickupY = 0;
+
     @Override
     public void runOpMode() {
-        
+        Pose2d startPose = new Pose2d(startX, startY, startHeading);
+        MecanumDrive drive = new MecanumDrive(hardwareMap, startPose);
+
+        TrajectoryActionBuilder move1 = drive.actionBuilder(startPose)
+                .strafeTo(new Vector2d(scorePreloadX, scorePreloadY))
+                .waitSeconds(3);
+
+//        TrajectoryActionBuilder move2 = drive.actionBuilder(startPose) End of previous trajectory how?
+//                .lineToYSplineHeading(pickupY, 0)
+//                .waitSeconds(3);
     }
+    
 }
