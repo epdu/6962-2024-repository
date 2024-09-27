aadfe260bbeba5a86bfd30779cdc9f337c32ad9e
nwilliams25 <nwilliams25@punahou.edu>
2024-09-26 21:18:10 -1000

    Added auto sequence to TestAuto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TestAuto.java
@@ -24,15 +24,27 @@ import java.util.Vector;
 @Autonomous(name = "Test Auto", group = "Test Autonomous")
 public class TestAuto extends LinearOpMode{
 
-    double startX = 0;
-    double startY = 0;
-    double startHeading = Math.toRadians(180);
+    double startX = 7;
+    double startY = -63.75;
+    double startHeading = Math.toRadians(-90);
     double scorePreloadX = 0;
-    double scorePreloadY = 0;
-    double pickupX = 0;
-    double pickupY = 0;
-    double parkX = 0;
-    double parkY = 0;
+    double scorePreloadY = -33;
+    double prepClipX = 62;
+    double prepClipY = -47;
+    double pickupClipX = 62;
+    double pickupCLipY = -62;
+    double scoreClipX = 8;
+    double scoreClipY = -33;
+    double intake1X = -48;
+    double intake1Y = -43;
+    double scoreBucketX = -57;
+    double scoreBucketY = -57;
+    double intake2X = -57;
+    double intake2Y = -43;
+    double prepParkX = -36;
+    double prepParkY = -12;
+    double parkX = -23;
+    double parkY = -12;
 
     @Override
     public void runOpMode() {
@@ -41,12 +53,31 @@ public class TestAuto extends LinearOpMode{
 
         TrajectoryActionBuilder move1 = drive.actionBuilder(startPose)
                 .strafeTo(new Vector2d(scorePreloadX, scorePreloadY))
-                .waitSeconds(3)
-                .lineToYLinearHeading(pickupY, 0)
-                .waitSeconds(3)
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(prepClipX, prepClipY))
+                .lineToY(pickupCLipY)
+                .waitSeconds(0.5)
+                .strafeTo(new Vector2d(scoreClipX, scoreClipY))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(intake1X, intake1Y))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(scoreBucketX, scoreBucketY))
+                .waitSeconds(0.5)
+                .strafeTo(new Vector2d(intake2X, intake2Y))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(scoreBucketX, scoreBucketY))
+                .waitSeconds(0.5)
+                .strafeTo(new Vector2d(intake2X, intake2Y))
+                .waitSeconds(1)
+                .strafeTo(new Vector2d(scoreBucketX, scoreBucketY))
+                .waitSeconds(0.5)
+                .strafeTo(new Vector2d(prepParkX, prepParkY))
                 .strafeTo(new Vector2d(parkX, parkY));
 
-        while (!isStarted() && !opModeIsActive()) {}
+        while (!isStarted() && !opModeIsActive()) {
+            telemetry.addLine("Initialized Red Side Auto");
+            telemetry.update();
+        }
 
         waitForStart();
 
