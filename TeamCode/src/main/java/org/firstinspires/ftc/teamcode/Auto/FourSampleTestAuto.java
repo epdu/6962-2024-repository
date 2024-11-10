f77f88de6163b03aea3b78f8a45e31837e82c54d
nwilliams25 <nwilliams25@punahou.edu>
2024-11-10 11:21:31 -1000

    cleaning up actions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/FourSampleTestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/FourSampleTestAuto.java
@@ -17,6 +17,7 @@ import org.firstinspires.ftc.teamcode.Subsystems.HorizontalSlides;
 import org.firstinspires.ftc.teamcode.Subsystems.IntakeArm;
 import org.firstinspires.ftc.teamcode.Subsystems.ScoringArm;
 import org.firstinspires.ftc.teamcode.Subsystems.VerticalSlides;
+import org.firstinspires.ftc.teamcode.Subsystems.SubsystemCommands;
 
 @Config
 @Autonomous(name = "0+4 Test Auto", group = "Autonomous", preselectTeleOp = "Solo Full Robot TeleOp")
@@ -42,16 +43,9 @@ public class FourSampleTestAuto extends LinearOpMode{
     public void runOpMode() {
         Pose2d startPose = new Pose2d(startX, startY, startHeading);
         MecanumDrive drive = new MecanumDrive(hardwareMap, startPose);
-
-        VerticalSlides verticalSlides = new VerticalSlides();
-        ScoringArm scoringArm = new ScoringArm();
-        IntakeArm intakeArm = new IntakeArm();
-        HorizontalSlides horizontalSlides = new HorizontalSlides();
+        SubsystemCommands subsystems = new SubsystemCommands();
 
         //defining movement trajectories
-        //TODO: Test heading with new action calling
-        //TODO: Test waitSeconds with all movement trajectories
-        //TODO: Add back in subsystem actions
         TrajectoryActionBuilder scorePreload = drive.actionBuilder(startPose)
                 .strafeToLinearHeading(new Vector2d(scoreBucketX, scoreBucketY), Math.toRadians(45));
 
@@ -77,20 +71,12 @@ public class FourSampleTestAuto extends LinearOpMode{
                 .strafeToLinearHeading(new Vector2d(parkX, parkY), Math.toRadians(90));
 
         while (!isStarted() && !opModeIsActive()) {
-            intakeArm.initialize(this);
-            verticalSlides.initialize(this);
-            scoringArm.initialize(this);
-            horizontalSlides.initialize(this);
-
+            subsystems.initialize(this);
 
             telemetry.addLine("Initialized Test 0+4 Auto");
             telemetry.update();
             Actions.runBlocking(
-                    new ParallelAction(
-                        intakeArm.IntakeTransfer(),
-                        scoringArm.WholeArmTransfer()
-
-                    )
+                    subsystems.INITIALIZE
             );
         }
 
@@ -99,7 +85,7 @@ public class FourSampleTestAuto extends LinearOpMode{
         if (isStopRequested()) return;
 
         //drive action builds
-        Action SCOREPRELOAD = scorePreload.build();
+        Action SCORE_PRELOAD = scorePreload.build();
         Action INTAKE1 = intake1.build();
         Action SCORE1 = score1.build();
         Action INTAKE2 = intake2.build();
@@ -108,72 +94,31 @@ public class FourSampleTestAuto extends LinearOpMode{
         Action SCORE3 = score3.build();
         Action PARK = park.build();
 
-        //subsystem actions
-        Action INTAKEANDTRANSFER =
-                new SequentialAction(
-                        intakeArm.IntakePickup(),
-                        new SleepAction(0.5),
-                        intakeArm.IntakeTransfer(),
-                        new SleepAction(0.2),
-                        horizontalSlides.HorizontalRetract(),
-                        new SleepAction(0.5),
-                        scoringArm.WholeArmTransfer(),
-                        intakeArm.ClawOpen(),
-                        new ParallelAction(
-                                verticalSlides.LiftUpToHighBucket(),
-                                scoringArm.ArmScoreBucket()
-                        )
-        );
-        Action EXTENDINTAKE =
-                new ParallelAction(
-                        intakeArm.IntakeHover(),
-                        horizontalSlides.HorizontalExtend()
-                );
-        Action SCOREBUCKET = scoringArm.DropBucket();
-        Action LIFTBUCKET =
-                new ParallelAction(
-                        verticalSlides.LiftUpToHighBucket(),
-                        scoringArm.ArmScoreBucket()
-                );
-        Action RETRACTVERTICAL =
-                new ParallelAction(
-                        scoringArm.StowWholeArm(),
-                        verticalSlides.Retract()
-                );
-        Action RETRACTALL =
-                new ParallelAction(
-                        verticalSlides.Retract(),
-                        horizontalSlides.HorizontalRetract(),
-                        scoringArm.StowArmClose(),
-                        intakeArm.IntakeTransfer()
-                );
-
-
         Actions.runBlocking(
                 new SequentialAction(
-//                            RETRACTALL,
-                        SCOREPRELOAD,
-//                            LIFTBUCKET,
-//                            SCOREBUCKET,
-//                            RETRACTVERTICAL,
+                            subsystems.RETRACT_ALL,
+                        SCORE_PRELOAD,
+                            subsystems.LIFT_BUCKET,
+                            subsystems.SCORE_BUCKET,
+                            subsystems.RETRACT_VERTICAL,
                         INTAKE1,
-//                            EXTENDINTAKE,
-//                            INTAKEANDTRANSFER,
+                            subsystems.EXTEND_INTAKE,
+                            subsystems.INTAKE_AND_TRANSFER,
                         SCORE1,
-//                            SCOREBUCKET,
-//                            RETRACTVERTICAL,
+                            subsystems.SCORE_BUCKET,
+                            subsystems.RETRACT_VERTICAL,
                         INTAKE2,
-//                            EXTENDINTAKE,
-//                            INTAKEANDTRANSFER,
+                            subsystems.EXTEND_INTAKE,
+                            subsystems.INTAKE_AND_TRANSFER,
                         SCORE2,
-//                            SCOREBUCKET,
-//                            RETRACTVERTICAL,
+                            subsystems.SCORE_BUCKET,
+                            subsystems.RETRACT_VERTICAL,
                         INTAKE3,
-//                            EXTENDINTAKE,
-//                            INTAKEANDTRANSFER,
+                            subsystems.EXTEND_INTAKE,
+                            subsystems.INTAKE_AND_TRANSFER,
                         SCORE3,
-//                            SCOREBUCKET,
-//                            RETRACTALL,
+                            subsystems.SCORE_BUCKET,
+                            subsystems.RETRACT_ALL,
                         PARK
                 )
         );
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/SubsystemCommands.java
@@ -0,0 +1,75 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.Util.RobotHardware;
+
+public class SubsystemCommands {
+    OpMode opmode;
+    RobotHardware rHardware;
+    VerticalSlides verticalSlides = new VerticalSlides();
+    HorizontalSlides horizontalSlides = new HorizontalSlides();
+    ScoringArm scoringArm = new ScoringArm();
+    IntakeArm intakeArm = new IntakeArm();
+
+    public void initialize(OpMode opmode) {
+        this.opmode = opmode;
+        rHardware.init(opmode.hardwareMap);
+
+        verticalSlides.initialize(opmode);
+
+        horizontalSlides.initialize(opmode);
+
+        scoringArm.initialize(opmode);
+
+        intakeArm.initialize(opmode);
+    }
+
+    public Action INTAKE_AND_TRANSFER =
+            new SequentialAction(
+                    intakeArm.IntakePickup(),
+                    new SleepAction(0.5),
+                    intakeArm.IntakeTransfer(),
+                    new SleepAction(0.2),
+                    horizontalSlides.HorizontalRetract(),
+                    new SleepAction(0.5),
+                    scoringArm.WholeArmTransfer(),
+                    intakeArm.ClawOpen(),
+                    new ParallelAction(
+                            verticalSlides.LiftUpToHighBucket(),
+                            scoringArm.ArmScoreBucket()
+                    )
+            );
+    public Action EXTEND_INTAKE =
+            new ParallelAction(
+                    intakeArm.IntakeHover(),
+                    horizontalSlides.HorizontalExtend()
+            );
+    public Action SCORE_BUCKET = scoringArm.DropBucket();
+    public Action LIFT_BUCKET =
+            new ParallelAction(
+                    verticalSlides.LiftUpToHighBucket(),
+                    scoringArm.ArmScoreBucket()
+            );
+    public Action RETRACT_VERTICAL =
+            new ParallelAction(
+                    scoringArm.StowWholeArm(),
+                    verticalSlides.Retract()
+            );
+    public Action RETRACT_ALL =
+            new ParallelAction(
+                    verticalSlides.Retract(),
+                    horizontalSlides.HorizontalRetract(),
+                    scoringArm.StowArmClose(),
+                    intakeArm.IntakeTransfer()
+            );
+    public Action INITIALIZE =
+            new ParallelAction(
+                    intakeArm.IntakeTransfer(),
+                    scoringArm.StowArmClose()
+            );
+}
