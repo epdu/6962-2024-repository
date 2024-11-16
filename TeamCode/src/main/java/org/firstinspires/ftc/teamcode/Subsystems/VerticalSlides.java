b6d598467094f7cc5a045cb5a42a1f2825f899e7
MisterKermit <97013614+MisterKermit@users.noreply.github.com>
2024-11-15 16:00:16 -1000

    Update VerticalSlides.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/VerticalSlides.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/VerticalSlides.java
@@ -274,8 +274,8 @@ public class VerticalSlides
             int error = rtpTarget - rightSlideMotor.getCurrentPosition();
             if (!initialized) {
                 int sign = (error >= 0 ? 1 : -1);
-                leftSlideMotor.setPower(sign * 0.9);
-                rightSlideMotor.setPower(sign * 0.9);
+                leftSlideMotor.setPower(sign * 0.7);
+                rightSlideMotor.setPower(sign * 0.7);
                 initialized = true;
             }
 
