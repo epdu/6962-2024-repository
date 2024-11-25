2409ac7f429a956342889ebc67660048d50d976f
jleung26 <jleung26@punahou.edu>
2024-11-24 19:08:02 -1000

    loop time testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/NewVerticalSlidesTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/NewVerticalSlidesTuning.java
@@ -4,6 +4,7 @@ package org.firstinspires.ftc.teamcode.TeleOp;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Subsystems.NewVerticalSlides;
@@ -12,12 +13,21 @@ import org.firstinspires.ftc.teamcode.Subsystems.NewVerticalSlides;
 public class NewVerticalSlidesTuning extends OpMode {
     private FtcDashboard dash = FtcDashboard.getInstance();
     private Telemetry dashboardTelemetry = dash.getTelemetry();
+    private ElapsedTime elapsedtime;
+
 
     private NewVerticalSlides verticalSlides = new NewVerticalSlides();
 
     @Override
     public void init() {
         verticalSlides.initialize(this);
+        elapsedtime = new ElapsedTime();
+
+    }
+
+    @Override
+    public void start() {
+        elapsedtime.reset();
     }
 
     @Override
@@ -27,6 +37,11 @@ public class NewVerticalSlidesTuning extends OpMode {
         dashboardTelemetry.addData("Right Motor Encoder Pos: ", verticalSlides.telemetryMotorPos());
         dashboardTelemetry.addData("Target: ", verticalSlides.telemetryTarget());
         dashboardTelemetry.addData("PID Power R: ", verticalSlides.telemetryOutput());
+        dashboardTelemetry.addData("elapsed time (loop time)", elapsedtime.milliseconds());
         dashboardTelemetry.update();
+
+        telemetry.addData("loop time", elapsedtime.milliseconds());
+        telemetry.update();
+        elapsedtime.reset();
     }
 }
