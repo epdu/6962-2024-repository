9d73efa90066fbaa505d50ea62364e42496dfcc3
MisterKermit <97013614+MisterKermit@users.noreply.github.com>
2024-12-13 15:06:29 -1000

    Correct intake arm values (new servo)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/IntakeArm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/IntakeArm.java
@@ -162,8 +162,8 @@ public class IntakeArm {
     public static class Arm {
         public Servo arm;
         public boolean isArmTransferring = true;
-        public static double armIntakeHoverPosition = 0.62;
-        public static double armIntakeGrabPosition = 1;
+        public static double armIntakeHoverPosition = 0.71;
+        public static double armIntakeGrabPosition = 0.93;
         public static double armTransferPosition = 0;
         public static double armIncrement = 0.001;
 
